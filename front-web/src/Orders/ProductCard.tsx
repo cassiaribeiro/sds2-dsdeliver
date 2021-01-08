1dcd61e601c10a112fc42098328875ffccfbf67e
Cassia Ribeiro <crra295@hotmail.com>
2021-01-08 15:57:23 -0300

    Connection with back-end

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,17 +1,35 @@
-import { ReactComponent as Pizza } from './pizza.svg';
-function ProductCard() {
+import { Product } from './types';
+
+type Props = {
+    product:  Product;
+}
+
+function formataPrice(price: number) {
+    const formatter =  new Intl.NumberFormat('pt-BR', {
+        style: 'currency', 
+        currency: 'BRL',
+        minimumFractionDigits: 2
+    });
+    return formatter.format(price);
+}
+
+function ProductCard({ product }: Props) {
     return (
         <div className="order-card-container">
             <h3 className="order-card-title">
-              Pizza calabresa  
+              {product.name}
             </h3>
-            <Pizza className="order-card-image"/>
+            <img 
+                src={product.immageUri}    
+                className="order-card-image"
+                alt={product.name}
+            />
             <h3 className="order-card-price">
-                33,90
+                {formataPrice(product.price)}
             </h3>
             <div className="order-card-description">
-                <h3>Descricao</h3>
-                <p>teste teste tset tsete </p>
+                <h3>Descrição</h3>
+                <p>{product.description} </p>
             </div>
         </div>
     )
